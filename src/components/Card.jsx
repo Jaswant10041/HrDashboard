70f87b35311b60edc478a52b115e225be43ee904
Jaswant10041 <jaswanthkumarmadineni@gmail.com>
2025-05-27 13:58:56 +0530

    latest3

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
   const { userData, isBookmarked, onBookmark } = props;
 
   return (
-    <div className="mt-8 flex flex-col p-2.5 text-base hover:border hover:border-black hover:cursor-pointer items-center bg-zinc-100 w-120 h-60 hover:rounded-none rounded-sm text-xl">
+    <div className="mt-8 flex flex-col p-2.5 text-base hover:border hover:border-black items-center bg-zinc-100 w-120 h-60 hover:rounded-none rounded-sm text-xl">
       <div className="flex w-full justify-between items-center">
         <img
           className="rounded w-10"
@@ -30,16 +30,11 @@ const Card = (props) => {
       <p>Age : {userData.age}</p>
       <p>Email : {userData.email}</p>
       <p className="font-medium">Department : {userData.company.department}</p>
-      <div className="rating">
-        <span data-twe-rating-icon-ref="1" className="text-yellow-400">★</span>
-        <span data-twe-rating-icon-ref="2" className="text-yellow-400">★</span>
-        <span data-twe-rating-icon-ref="3" className="text-yellow-400">★</span>
-        <span data-twe-rating-icon-ref="4" className="text-yellow-400">★</span>
-        <span data-twe-rating-icon-ref="5" className="text-yellow-400">★</span>
+      <div className="pt-2 hover:bg-amber-50 cursor-pointer">
+        <Link to={`/employee/${userData?.id}`}>
+          <label className="border-1 p-1 rounded-xl cursor-pointer">View</label>
+        </Link>
       </div>
-      <Link to={`/employee/${userData?.id}`}>
-      View
-      </Link>
     </div>
   );
 };
